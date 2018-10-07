bfd92e5e6700c1a380ee9e009f93819a41b0342e
Serhat Selim <serhatselim@hotmail.com.tr>
2018-10-07 22:07:09 +0300

    json parse and binding dom
    completed

--- readLocalJSON/src/app/app.component.ts
+++ readLocalJSON/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { JsonFileServiceService } from './json-file-service.service';
+import { stringify } from '@angular/core/src/render3/util';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,26 @@ import { JsonFileServiceService } from './json-file-service.service';
 })
 export class AppComponent {
   title = 'readLocalJSON';
+  public documentationElement = {
+    id: String,
+    icon: String,
+    title: String,
+    subtitles: [{
+      title: String,
+      body: String,
+      imgPath: String
+    }]
+  };
   /**
    *
    */
   constructor(private documentationService: JsonFileServiceService) {
-    this.documentationService.getDocumentationData().subscribe(data => {
-      console.log(data);
+    this.documentationService.getDocumentationData().subscribe(result => {
+      console.log(result);
+      this.documentationElement =  JSON.parse(JSON.stringify(result.data[0]));
+       console.log('*********', this.documentationElement);
+       console.log('subtitles.imgPath', this.documentationElement.subtitles[0].imgPath);
+
   });
   }
 }
